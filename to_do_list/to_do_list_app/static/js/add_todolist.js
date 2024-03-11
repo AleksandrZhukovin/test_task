2cfe7de9ebfcc19c4e2a3ea6ef1dcdc8b20a9a6a
AleksandrZhukovin <sashokzuk@gmail.com>
2024-03-11 20:59:56 +0200

    add(static): add static files (base css, js for project create)

---
+++ to_do_list/to_do_list_app/static/js/add_todolist.js
@@ -0,0 +1,38 @@
+$(function(){
+    $('#addToDoListBtn').click(function(){
+        document.getElementById('main').inert = true;
+        document.getElementById('addToDoList').show();
+        document.getElementById('addToDoList').classList.add('dialog_appear');
+    })
+})
+
+$(function(){
+    $('#closeAddToDoList').click(function(){
+        document.getElementById('main').inert = false;
+        document.getElementById('addToDoList').close();
+        document.getElementById('addToDoList').classList.remove('dialog_appear');
+    })
+})
+
+$(function(){
+    $('#sendToDoList').click(function(){
+        $.ajax('/add_to_do_list/', {
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val(),
+                'name': $('#projectName').val()
+            },
+            'success': function(data){
+                if (document.getElementById('message')) {
+                    document.getElementById('message').remove();
+                }
+                document.getElementById('projects').innerHTML = data + document.getElementById('projects').innerHTML;
+                document.getElementById('main').inert = false;
+                document.getElementById('addToDoList').close();
+                $('#projectName').val('');
+            }
+        })
+    })
+})
